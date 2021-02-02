810da6a3f4d3c6be8a7e90e0bd4a3090257b4d96
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-02 16:48:34 -0600

    post class edits

--- scripts/main.js
+++ scripts/main.js
@@ -15,16 +15,18 @@ function generateDiv() {
   clickCount++;
   // append a new div to the existing container
   // also add a button to turn the div yellow and a delete button
-  $('.div-container').append(` <div class="red-div">
-  <p class="click-count">${clickCount}</p>
-  <button class="yellow-button">Yellow</button>
-  <button class="delete-button">Delete</>
-</div>`);
+  $('.div-container').append(`
+  <div class="red-div">
+    <p>${clickCount}</p>
+    <button class="yellow-button">Yellow</button>
+    <button class="delete-button">Delete</>
+  </div>
+`);
 }
 
 // function that targets the div of which the yellow button was pressed and changes the background to yellow
 function yellowBackground() {
-  $(this).parent().attr('class', 'yellow-div');
+  $(this).parent().addClass('yellow-div');
 }
 
 // function that targets the div of which the delete button was pressed and removes it from the DOM
